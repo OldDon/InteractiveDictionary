3c2dc03f4cd6a7e1160b1f76a80d7e9246b55c44
Dave Elliott <davey.elliott@gmail.com>
2018-05-28 15:07:42 +0100

    Added code to check for capitalisation of proper nouns

--- InteractiveDictionary/interactive_dictionary_2.py
+++ InteractiveDictionary/interactive_dictionary_2.py
@@ -6,15 +6,22 @@ data = json.load(open("data.json"))
 def translate(w):
     w = w.lower()
     if w in data:
-        return data(w)
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y/N: " % get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y/N: " % get_close_matches(w, data.keys())[0])
         if yn == "y":
-            return data[get_close_matches(w, data.keys()))[0]]
+            return data[get_close_matches(w, data.keys())[0]]
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
 
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
